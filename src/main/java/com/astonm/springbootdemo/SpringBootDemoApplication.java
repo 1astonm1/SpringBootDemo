1f1cedde578c031a79fc56e9af418e2a70229d28
astonm <184469824@qq.com>
2021-07-03 14:22:54 +0800

    引入swagger 配置数据库

--- src/main/java/com/astonm/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/astonm/springbootdemo/SpringBootDemoApplication.java
@@ -2,12 +2,20 @@ package com.astonm.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class SpringBootDemoApplication {
+public class SpringBootDemoApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDemoApplication.class, args);
     }
+
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/doc.html").addResourceLocations("classpath*:/META-INF/resources/");
+//        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath*:/META-INF/resources/webjars/");
+//    }
 
 }
